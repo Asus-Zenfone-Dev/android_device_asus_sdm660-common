74a7324155b798080c0b22f1a7faf346bba594d8
nguyenhung9x2020 <nguyenhung9x2018@gmail.com>
2021-10-06 09:03:30 +0700

    sdm660-common: Drop CABC via livedisplay

--- sdm660.mk
+++ sdm660.mk
@@ -249,8 +249,7 @@ PRODUCT_COPY_FILES += \
 
 # LiveDisplay native
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm \
-    vendor.lineage.livedisplay@2.0-service-sysfs
+    vendor.lineage.livedisplay@2.0-service-sdm
 
 # Media
 PRODUCT_COPY_FILES += \
