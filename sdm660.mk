a0f0d89ce212706e1737218042b1fb6fcac02ca6
nguyenhung9x2020 <nguyenhung9x2018@gmail.com>
2021-10-06 09:00:32 +0700

    sdm660-common: Drop light

--- sdm660.mk
+++ sdm660.mk
@@ -246,9 +246,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
-# Lights
-PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.xiaomi_sdm660
 
 # LiveDisplay native
 PRODUCT_PACKAGES += \
